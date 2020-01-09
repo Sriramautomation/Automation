d0ae50d08c3d6c831735e3ac1c8b29810f295949
Sriramautomation <srirammaqkw@gmail.com>
2020-01-09 08:25:03 +0530

    added new class

---
+++ src/test/java/org/mavenprac/TestGit.java
@@ -0,0 +1,10 @@
+package org.mavenprac;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver","https://github.com/Sriramautomation/Automation.git");
+
+	}
+
+}
